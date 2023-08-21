84829876bb0afa9c2d08bd4653095e72fd9f72e7
isaiah <isaiahsinnathamby@gmail.com>
2023-08-20 22:11:17 -0400

    implemented Link and rerouting to BookingPage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom'
 import hero_dish from '../../assets/images/lemon-garlic-chicken.jpeg';
 import './Header.css';
 
 export default function Header() {
+
     return (
         <header>
             <div className='hero-section'>
@@ -9,7 +11,10 @@ export default function Header() {
                     <h1 className='title'>Little Lemon</h1>
                     <h2 className='location'>Chicago</h2>
                     <h3 className='description'>Nestled in the heart of the city, Little Lemon is more than just a restaurant. It's a place where the simplicity of ingredients, the love of food, and the joy of sharing all blend into one delightful experience. With an ambiance that whispers of warm, homey comfort, Little Lemon provides a getaway from the bustle of daily life.</h3>
-                    <button className='reserve-table-button'>Reserve a Table</button>
+
+                    <Link to='/booking'>
+                        <button className='reserve-table-button'>Reserve a Table</button>
+                    </Link>
                 </div>
 
                 <div className='hero-image-container'>
