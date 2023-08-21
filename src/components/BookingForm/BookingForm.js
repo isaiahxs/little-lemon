6cfc1b04137dc57d94bcfd618960d752130deec9
isaiah <isaiahsinnathamby@gmail.com>
2023-08-21 08:16:37 -0400

    added booking form

--- src/components/BookingForm/BookingForm.js
+++ src/components/BookingForm/BookingForm.js
@@ -1,9 +1,35 @@
+import { useState } from 'react';
 import './BookingForm.css';
 
 export default function BookingForm() {
+    const [date, setDate] = useState('');
+    const [time, setTime] = useState('');
+    const [guests, setGuests] = useState('');
+    const [occasion, setOccasion] = useState('');
+
     return (
-        <>
-            booking form
-        </>
+        <div className='form-section'>
+            <form className='booking-form'>
+                <label htmlFor="res-date">Choose date</label>
+                <input type="date" id="res-date" value={date} onChange={e => setDate(e.target.value)} />
+                <label htmlFor="res-time">Choose time</label>
+                <select id="res-time" value={time} onChange={e => setTime(e.target.value)}>
+                    <option>17:00</option>
+                    <option>18:00</option>
+                    <option>19:00</option>
+                    <option>20:00</option>
+                    <option>21:00</option>
+                    <option>22:00</option>
+                </select>
+                <label htmlFor="guests">Number of guests</label>
+                <input type="number" placeholder="1" id="guests" min="1" max="10" value={guests} onChange={e => setGuests(e.target.value)} />
+                <label htmlFor="occasion">Occasion</label>
+                <select id="occasion" value={occasion} onChange={e => setOccasion(e.target.value)}>
+                    <option>Birthday</option>
+                    <option>Anniversary</option>
+                </select>
+                <input type="submit" value="Make Your reservation" />
+            </form>
+        </div>
     )
 }
